7cc0f14f34165a400853d2772885158f0f6d3b1b
rritoch <rritoch@gmail.com>
2015-01-01 05:14:32 +0800

    Added conditional compilation

--- src/leiningen/sablecc/compile.clj
+++ src/leiningen/sablecc/compile.clj
@@ -2,10 +2,13 @@
   (:require [clojure.java.io :as io]
             [robert.hooke]
             [leiningen.compile :as lein-compile]
-            [leiningen.javac :as lein-javac])
+            [leiningen.javac :as lein-javac]
+            [clojure.string :as string])
   (:refer-clojure :exclude [compile])
   (:import [org.sablecc.sablecc SableCC]))
 
+(def javac-hook-run (atom false))
+(def javac-hook-activated (atom false))
 
 (defn sablecc-source-paths
   [project]
@@ -23,27 +26,48 @@
                      paths)))))
                             
 
+(defn get-sablecc-package
+  [f]
+    (when-let [r (re-find #"(s?)(^|\n)\QPackage\E(\s+)([^;\s]*)"  
+                          (string/replace (slurp f) 
+                                          #"(s?)/\*(.|\r\n)*?\*/" ""))]
+      (nth r 4)))
+           
+
 (defn destination-path
   [project]
     (str (:target-path project) "/generated-sources/sablecc/"))
 
-(def javac-hook-run (atom false))
-(def javac-hook-activated (atom false))
+(defn need-compile?
+  [project src]
+    (when-let [pkg (get-sablecc-package src)]
+      (let [pp (io/file (str (destination-path project)
+                             "/"
+                             (string/replace pkg "." "/")
+                             "/parser/Parser.java"))]
+           (or (not (.isFile pp)) 
+               (> (.lastModified src) (.lastModified pp))))))
+
+(defn gen-sources
+  [project]
+   (let [target-path (destination-path project)
+         target (io/file target-path)]
+        (.mkdirs target)
+        (.mkdir target)
+        (doseq [s (sources project)]
+               (if (need-compile? project s)
+                   (SableCC/processGrammar s target)))))
 
 (defn javac-hook
   [f project & args]
     (if @javac-hook-run
         (apply f (into [project] args))
         (do (reset! javac-hook-run true)
-          (let [target-path (destination-path project)
-                target (io/file target-path)]
-             (.mkdirs target)
-             (.mkdir target)
-             (doseq [s (sources project)]
-              (SableCC/processGrammar s target))
-              (if args
-                  (apply f (into [(assoc project :java-source-paths [target-path])] args))
-                  (apply f [(assoc project :java-source-paths [target-path])]))
+          (let [target-path (destination-path project)]
+             (gen-sources project)
+             (if args
+                 (apply f (into [(assoc project :java-source-paths [target-path])] args))
+                 (apply f [(assoc project :java-source-paths [target-path])]))
              (apply f (into [project] args))))))
 
 (defn activate
@@ -57,8 +81,10 @@
 (defn compile
   "Compile SableCC sources"
   [project]
-    (activate project)
-    (lein-javac/javac project) ; Runs javac-hook
+    (reset! javac-hook-run true) ; Stop hook
+    (gen-sources project)
+    (lein-javac/javac (assoc project :java-source-paths [(destination-path project)]))
+    (lein-javac/javac project)
     (lein-compile/compile project))
   
 
