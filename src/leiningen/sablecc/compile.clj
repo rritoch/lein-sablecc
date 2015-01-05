3b1b04910c856c0e3e7c287984e0ffeddfde325e
rritoch <rritoch@gmail.com>
2015-01-05 15:17:56 +0800

    Added generated .dat files to :targetpath /classes folder

--- src/leiningen/sablecc/compile.clj
+++ src/leiningen/sablecc/compile.clj
@@ -133,31 +133,59 @@
            (clean-path pp))))
 
 (defn gen-sources
+  "Generate sources returning list of modified packages"
   [project]
    (let [target-path (destination-path project)
-         target (io/file target-path)]
+         target (io/file target-path)
+         cc (fn [s]
+                (let [ss (.toString s)]
+                    (when (need-compile? project s)
+                        (eval-in-project project `(org.sablecc.sablecc.SableCC/processGrammar (clojure.java.io/file ~ss) (clojure.java.io/file ~target-path))
+                                       '(require 'clojure.java.io))
+                        
+                        (get-sablecc-package s))))]
         (doseq [s (sources project)]
                   (if (need-compile? project s)
                       (clean-gen-sources project s)))
         (.mkdirs target)
         (.mkdir target)
-        (doseq [s (sources project)]
-             (let [ss (.toString s)]
-                    (if (need-compile? project s)
-                        (eval-in-project project `(org.sablecc.sablecc.SableCC/processGrammar (clojure.java.io/file ~ss) (clojure.java.io/file ~target-path))
-                                       '(require 'clojure.java.io)))))))
+        (doall (keep cc (sources project)))))
+
+(defn cp
+  [src dest]
+     (let [fin (io/file src)
+           fout (io/file dest)]
+       (.mkdirs (.getParentFile fout))
+       (io/copy fin fout)))
+
+(defn after-compile-gen-sources
+  [project pkg]
+    (let [pkg-path (string/replace pkg "." "/")
+          gs-path (str (destination-path project)
+                       "/"
+                        pkg-path)
+          t-path (str (:target-path project)
+                      "/classes/"
+                      pkg-path)
+          rlist ["/parser/parser.dat"
+                 "/lexer/lexer.dat"]]
+         (doseq [f rlist]
+           (cp (str gs-path f)
+               (str t-path f)))))
 
 (defn javac-hook
   [f project & args]
     (if @javac-hook-run
         (apply f (into [project] args))
         (do (reset! javac-hook-run true)
-          (let [target-path (destination-path project)]
-             (gen-sources project)
+          (let [target-path (destination-path project)
+                modified-packages (gen-sources project)]
              (if args
                  (apply f (into [(assoc project :java-source-paths [target-path])] args))
                  (apply f [(assoc project :java-source-paths [target-path])]))
-             (apply f (into [project] args))))))
+             (doseq [pkg modified-packages]
+                (after-compile-gen-sources project pkg) 
+             (apply f (into [project] args)))))))
 
 (defn activate
   "Activate SableCC javac hook"
@@ -171,9 +199,9 @@
   "Compile SableCC sources"
   [project]
     (reset! javac-hook-run true) ; Stop hook
-    (gen-sources project)
-    (lein-javac/javac (assoc project :java-source-paths [(destination-path project)]))
+    (let [modified-packages (gen-sources project)]
+       (lein-javac/javac (assoc project :java-source-paths [(destination-path project)]))
+       (doseq [pkg modified-packages]
+           (after-compile-gen-sources project pkg)))
     (lein-javac/javac project)
     (lein-compile/compile project))
-  
-
